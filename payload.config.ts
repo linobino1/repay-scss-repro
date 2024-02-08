f6f85e83c1de019cebe0fc0b12d8aad2509f78b8
Leo Hilsheimer <76655725+linobino1@users.noreply.github.com>
2024-02-08 12:46:10 +0100

    Initial commit

---
+++ payload.config.ts
@@ -0,0 +1,28 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { viteBundler } from "@payloadcms/bundler-vite";
+import { buildConfig } from "payload/config";
+import path from "path";
+import Users from "./cms/collections/Users";
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: viteBundler(),
+    vite: (incomingViteConfig) => ({
+      ...incomingViteConfig,
+      build: {
+        ...incomingViteConfig.build,
+        emptyOutDir: false,
+      },
+    }),
+  },
+  editor: lexicalEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI ?? false,
+  }),
+  collections: [Users],
+  typescript: {
+    outputFile: path.resolve(__dirname, "cms/payload-types.ts"),
+  },
+});
